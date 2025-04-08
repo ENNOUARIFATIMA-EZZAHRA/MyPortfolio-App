5fc1077f6c99613b6cfbc6bc7aa9934cada6b704
ENNOUARI FATIMA-EZZAHRA <fatimaezzahraennouari@gmail.com>
2025-04-08 10:55:28 +0100

    created project angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: './app.component.html',
 })
 export class AppComponent {
   title = 'MyPortfolio';
